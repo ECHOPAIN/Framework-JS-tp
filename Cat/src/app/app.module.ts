c0984364d7fcf78d71db5d2296babfa5509d5929
martel <benjamin.martel.etu@univ-lille.fr>
2020-11-29 11:44:20 +0100

    avancement cat app

--- Cat/src/app/app.module.ts
+++ Cat/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardlistComponent } from './cardlist/cardlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardlistComponent
   ],
   imports: [
     BrowserModule,
