76df2adbd7fa1345cf39b2ac3bc32e1cddafde9d
julien <julien-9871@hotmail.fr>
2020-12-01 16:19:42 +0100

    update

--- Cat/src/app/cardlist/cardlist.component.ts
+++ Cat/src/app/cardlist/cardlist.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardlistComponent implements OnInit {
 
+  public initialCards = [
+    {
+      id: 1,
+      title: 'Random cat card',
+      imageUrl: 'https://cataas.com/cat?width=250&height=200',
+      description: 'That card shows a random cat image.'
+    },
+    {
+      id: 2,
+      title: 'Random cat card',
+      imageUrl: 'https://cataas.com/cat/says/Hello?width=250&height=200',
+      description: 'That card shows a random cat image with a text !'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
